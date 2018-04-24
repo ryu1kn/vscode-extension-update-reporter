50c9f1ad528d1c6aba859ccd0b2af9885760f7bf
Ryuichi Inagaki <ryuichi.inagaki.24@googlemail.com>
2018-04-24 23:04:35 +1000

    Use name if displayName is not available

--- test/lib/extension.test.js
+++ test/lib/extension.test.js
@@ -3,12 +3,20 @@ const assert = require('assert')
 const Extension = require('../../lib/extension')
 
 describe('Extension', () => {
-  const rawExtension = {
-    packageJSON: { version: '0.0.2' }
-  }
-  const extension = new Extension({ raw: rawExtension })
-
-  it('finds changelogs', async () => {
-    assert.ok(extension.isUpdated('0.0.1'))
+  it('uses displayName as its name', async () => {
+    const extension = createExtension({ displayName: 'foo', name: 'bar' })
+    assert.deepEqual(extension.displayName, 'foo')
   })
+
+  it('uses name as its name if displayName is not available', async () => {
+    const extension = createExtension({ name: 'bar' })
+    assert.deepEqual(extension.displayName, 'bar')
+  })
+
+  function createExtension (packageJson) {
+    const rawExtension = {
+      packageJSON: packageJson
+    }
+    return new Extension({ raw: rawExtension })
+  }
 })
