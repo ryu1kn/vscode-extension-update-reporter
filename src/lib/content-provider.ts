0fc0686a3808bda12d6c306f5b8b4e9fda1299e1
Ryuichi Inagaki <ryuichi.inagaki.24@googlemail.com>
2018-04-29 14:25:31 +1000

    ContentProvider implements vscode.TextDocumentContentProvider

--- src/lib/content-provider.ts
+++ src/lib/content-provider.ts
@@ -1,15 +1,16 @@
 import HtmlReportGenerator from './html-report-generator';
 import ExtensionUpdatesReportGenerator from "./extension-updates-report-generator";
+import * as vscode from "vscode";
 const htmlReportGenerator = new HtmlReportGenerator();
 
-export default class ContentProvider {
+export default class ContentProvider implements vscode.TextDocumentContentProvider {
   private extensionUpdatesReportGenerator: ExtensionUpdatesReportGenerator;
 
   constructor (params: any) {
     this.extensionUpdatesReportGenerator = params.extensionUpdatesReportGenerator;
   }
 
-  async provideTextDocumentContent (): Promise<string> {
+  async provideTextDocumentContent () {
     const markdownChangelog = await this.extensionUpdatesReportGenerator.generate();
     return htmlReportGenerator.generate(markdownChangelog);
   }
