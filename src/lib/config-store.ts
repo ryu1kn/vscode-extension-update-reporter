db5eae08c02808794bd038b12e325125cf6ba4a5
Ryuichi Inagaki <ryuichi.inagaki.24@googlemail.com>
2018-05-01 23:40:42 +1000

    ConfigStore is responsible for encoding/decoding values from configuration

--- src/lib/config-store.ts
+++ src/lib/config-store.ts
@@ -1,8 +1,14 @@
 import {EXTENSION_ID} from './const';
 import * as vscode from 'vscode';
-import {ObjectMap} from './utils';
+import {mapObject, ObjectMap} from './utils';
+import {parseVersion, Version} from './entities/version';
+import {PreloadedExtension} from './entities/extension';
 
 const EXTENSION_VERSION_MAP = 'lastCheckedVersions';
+
+export type ExtensionVersionMap = {
+  [extensionId: string]: Version
+};
 
 export default class ConfigStore {
   private vscWorkspace: any;
@@ -11,15 +17,25 @@ export default class ConfigStore {
     this.vscWorkspace = vscWorkspace;
   }
 
-  get lastCheckedVersions(): ObjectMap {
-    return this.extensionConfig.get(EXTENSION_VERSION_MAP) || {};
+  get lastCheckedVersions(): ExtensionVersionMap {
+    const versionMap = this.extensionConfig.get(EXTENSION_VERSION_MAP) || {};
+    return mapObject(versionMap, parseVersion);
   }
 
   private get extensionConfig(): vscode.WorkspaceConfiguration {
     return this.vscWorkspace.getConfiguration(EXTENSION_ID);
   }
 
-  async updateLastCheckedVersions(lastCheckedVersions: ObjectMap) {
-    return this.extensionConfig.update(EXTENSION_VERSION_MAP, lastCheckedVersions, true);
+  async updateLastCheckedVersions(lastCheckedVersions: PreloadedExtension[]) {
+    const versionMap = this.getExtensionVersionMap(lastCheckedVersions);
+    return this.extensionConfig.update(EXTENSION_VERSION_MAP, versionMap, true);
+  }
+
+  private getExtensionVersionMap (extensions: PreloadedExtension[]): ObjectMap {
+    return extensions.reduce(
+      (map, extension) =>
+        Object.assign({}, map, { [extension.id]: extension.version.toString() }),
+      {}
+    );
   }
 }
--- src/lib/extension-store.ts
+++ src/lib/extension-store.ts
@@ -1,11 +1,5 @@
 import {PreloadedExtension, RawExtension} from './entities/extension';
-import {parseVersion, Version} from './entities/version';
-import {mapObject, ObjectMap} from './utils';
 import ConfigStore from './config-store';
-
-export type ExtensionVersionMap = {
-  [extensionId: string]: Version
-};
 
 export default class ExtensionStore {
   private configStore: ConfigStore;
@@ -15,12 +9,8 @@ export default class ExtensionStore {
     this.configStore = configStore;
   }
 
-  private get lastCheckedVersions (): ExtensionVersionMap {
-    return mapObject(this.configStore.lastCheckedVersions, parseVersion);
-  }
-
   memoLoadedExtensions (extensions: RawExtension[]): void {
-    const versionMap = this.lastCheckedVersions;
+    const versionMap = this.configStore.lastCheckedVersions;
     this.loadedExtensions = extensions.map(
       extension => extension.withPrevInstalledVersion(versionMap[extension.id])
     );
@@ -35,15 +25,6 @@ export default class ExtensionStore {
   }
 
   async persistLoadedExtensions(): Promise<void> {
-    const versionMap = this.getExtensionVersionMap(this.loadedExtensions);
-    await this.configStore.updateLastCheckedVersions(versionMap);
-  }
-
-  private getExtensionVersionMap (extensions: PreloadedExtension[]): ObjectMap {
-    return extensions.reduce(
-      (map, extension) =>
-        Object.assign({}, map, { [extension.id]: extension.version.toString() }),
-      {}
-    );
+    await this.configStore.updateLastCheckedVersions(this.loadedExtensions);
   }
 }
