ba8925c63c338e0099c920e5989a679ed77e68c1
Jeroen van Warmerdam <jeronevw@hotmail.com>
2022-09-26 13:54:10 +0200

    Fix linting

--- src/lib/markdown-report-builder.ts
+++ src/lib/markdown-report-builder.ts
@@ -36,10 +36,12 @@ export default class MarkdownReportBuilder {
   private buildExtensionLinks(extension: LoadedExtension): string {
     const links: string[] = [];
     links.push(`[Marketplace](https://marketplace.visualstudio.com/items/${extension.id})`);
-    if(O.isSome(extension.changelog))
+    if (O.isSome(extension.changelog)) {
       links.push(`[Changelog](https://marketplace.visualstudio.com/items/${extension.id}/changelog)`);
-    if(extension.homepage)
+    }
+    if (extension.homepage) {
       links.push(`[Homepage](${extension.homepage})`);
+    }
     return links.join(' ● ');
   }
 
